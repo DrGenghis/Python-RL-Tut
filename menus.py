311c7e22a050bf193f86494ff157c80706ea81f7
DrGenghis <nebbulawalker@gmail.com>
2017-08-20 15:10:30 -0700

    End of tutorial following
    Finished the tutorial series so everything from here on is custom.
    
    Equipment and dungeon scaling is now in.
    
    BUGS:
    - Cannot equip gear. If you try it crashes the game.

--- menus.py
+++ menus.py
@@ -63,12 +63,20 @@ def character_screen(player, character_screen_width, character_screen_height, sc
 	y = screen_height // 2 - character_screen_height // 2
 	libtcod.console_blit(window, 0, 0, character_screen_width, character_screen_height, 0, x, y, 1.0, 0.7)
 	
-def inventory_menu(con, header, inventory, inventory_width, screen_width, screen_height):
+def inventory_menu(con, header, player, inventory_width, screen_width, screen_height):
 	# Show a menu with each item of the inventory as an option
-	if len(inventory.items) == 0:
+	if len(player.inventory.items) == 0:
 		options = ['Inventory is empty.']
 	else:
-		options = [item.name for item in inventory.items]
+		options = []
+		
+		for item in player.inventory.items:
+			if player.equipment.main_hand == item:
+				options.append('{0} (on main hand)'.format(item.name))
+			elif player.equipment.off_hand == item:
+				options.append('{0} (on off hand)'.format(item.name))
+			else:
+				options.append(item.name)
 		
 	menu(con, header, options, inventory_width, screen_width, screen_height)
 	
